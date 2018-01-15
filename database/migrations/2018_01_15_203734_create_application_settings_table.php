fe8cd346bf105abf4858c135a625adf12925e9ed
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-15 20:53:13 -0300

    Created the Application Setting Model

---
+++ database/migrations/2018_01_15_203734_create_application_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Creates the application_settings table
+ */
+class CreateApplicationSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application_settings', function (Blueprint $table) {
+            $table->string('key');
+            $table->string('value');
+            $table->primary(['key', 'value']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application_settings');
+    }
+}
