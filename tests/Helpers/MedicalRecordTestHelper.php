0e9f632769385f963de69ad1fdbd7677482d492e
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-16 15:05:06 -0300

    Adding MedicalRecord Test Helper trait

---
+++ tests/Helpers/MedicalRecordTestHelper.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Helpers;
+
+use App\MedicalRecord;
+
+/**
+ * Add convenient methods to make it easier to work with the MedicalRecord Model in tests
+ */
+trait MedicalRecordTestHelper
+{
+    /**
+     * Saves a new MedicalRecord to the database and returns it
+     * @param array $overrides
+     * @return MedicalRecord
+     */
+    protected function createMedicalRecord(array $overrides = [])
+    {
+        return factory(MedicalRecord::class)->create($overrides);
+    }
+}
