edf2c599154530ed18df3b9f615af4c5f31e937e
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-16 17:42:14 -0300

    Extracted PatientsPolicy beheaviour into abstract ModelPolicy class

---
+++ app/Policies/ModelPolicy.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Str;
+
+/**
+ * Handles Authorization for the Application Models
+ * It's RESTful based, so it will check for view, create, update and delete actions
+ * The authorization is delegated to the User Permissions.
+ * It expect a convention on the Permissions Names: modelName-actionName
+ */
+abstract class ModelPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * The name of the Model that the Policy refers to
+     * @var string
+     */
+    private $modelName = null;
+
+    /**
+     * Should return the name of the Model that the Policy handles
+     * @return string
+     */
+    protected function modelName()
+    {
+        if (is_null($this->modelName)) {
+            $reflect = new \ReflectionClass($this);
+            $temp = str_replace('Policy', '', $reflect->getShortName());
+            $this->modelName = Str::singular($temp);
+        }
+
+        return $this->modelName;
+    }
+
+    /**
+     * Determine whether the User can view the patient.
+     *
+     * @param  \App\User  $user
+     * @return bool
+     */
+    public function view(User $user)
+    {
+        return $user->hasPermission("{$this->modelName()}-View");
+    }
+
+    /**
+     * Determine whether the User can create patients.
+     *
+     * @param  \App\User  $user
+     * @return bool
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermission("{$this->modelName()}-Create");
+    }
+
+    /**
+     * Determine whether the User can update the patient.
+     *
+     * @param  \App\User  $user
+     * @return bool
+     */
+    public function update(User $user)
+    {
+        return $user->hasPermission("{$this->modelName()}-Update");
+    }
+
+    /**
+     * Determine whether the User can delete the patient.
+     *
+     * @param  \App\User  $user
+     * @return bool
+     */
+    public function delete(User $user)
+    {
+        return $user->hasPermission("{$this->modelName()}-Delete");
+    }
+}
--- app/Policies/PatientsPolicy.php
+++ app/Policies/PatientsPolicy.php
@@ -2,57 +2,9 @@
 
 namespace App\Policies;
 
-use App\User;
-use Illuminate\Auth\Access\HandlesAuthorization;
-
 /**
  * Handles authorization for the Patient Model
  */
-class PatientsPolicy
+class PatientsPolicy extends ModelPolicy
 {
-    use HandlesAuthorization;
-
-    /**
-     * Determine whether the user can view the patient.
-     *
-     * @param  \App\User  $user
-     * @return bool
-     */
-    public function view(User $user)
-    {
-        return $user->hasPermission('Patient-View');
-    }
-
-    /**
-     * Determine whether the user can create patients.
-     *
-     * @param  \App\User  $user
-     * @return bool
-     */
-    public function create(User $user)
-    {
-        return $user->hasPermission('Patient-Create');
-    }
-
-    /**
-     * Determine whether the user can update the patient.
-     *
-     * @param  \App\User  $user
-     * @return bool
-     */
-    public function update(User $user)
-    {
-        return $user->hasPermission('Patient-Update');
-    }
-
-    /**
-     * Determine whether the user can delete the patient.
-     *
-     * @param  \App\User  $user
-     * @return bool
-     */
-    public function delete(User $user)
-    {
-        return $user->hasPermission('Patient-Delete');
-    }
 }
