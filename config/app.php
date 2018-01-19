96226629fc123b63e3f5fc58cbdc5c2b083e0dab
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-19 16:55:03 -0300

    Removed HTML & Forms package

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -232,9 +231,7 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-        
-        'Form'         => Collective\Html\FormFacade::class,
-        'Html'         => Collective\Html\HtmlFacade::class,
+
     ],
 
 ];
