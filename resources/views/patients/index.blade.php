ea0ca611f6dd88c91062963db14c1bd025277fe7
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-20 19:07:42 -0300

    Patients@index: - Renamed DNI colum to Identification - Renamed View colum to Details

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -11,13 +11,13 @@
                 <th><abbr title="Number">#</abbr></th>
                 <th>Name</th>
                 <th>Last Name</th>
-                <th>DNI</th>
+                <th>Identification</th>
                 <th>Birth Date</th>
                 <th>Gender</th>
                 <th>Medical Insurance</th>
                 <th>Address</th>
                 <th>Phone</th>
-                <th>View</th>
+                <th>Details</th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
