5be852d54b6397ddc2915720bf59bfd124994c1b
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-20 13:44:46 -0300

    Patients@index: Removed unnecesary columns

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -14,15 +14,9 @@
                 <th>DNI</th>
                 <th>Birth Date</th>
                 <th>Gender</th>
+                <th>Medical Insurance</th>
                 <th>Address</th>
                 <th>Phone</th>
-                <th>Medical Insurance</th>
-                <th>Home Type</th>
-                <th>Heating Type</th>
-                <th>Water Type</th>
-                <th>Refrigerator?</th>
-                <th>Electricity?</th>
-                <th>Pets?</th>
                 <th>View</th>
                 <th>Edit</th>
                 <th>Delete</th>
@@ -37,15 +31,9 @@
                     <td>{{ $patient->document }}</td>
                     <th>{{ $patient->birth_date->toDateString() }}</th>
                     <td>{{ $patient->gender }}</td>
+                    <td>{{ $patient->medicalInsurance->value() }}</td>
                     <td>{{ $patient->address }}</td>
                     <td>{{ $patient->phone }}</td>
-                    <td>{{ $patient->medicalInsurance->value() }}</td>
-                    <th>{{ $patient->homeType->value() }}</th>
-                    <td>{{ $patient->heatingType->value() }}</td>
-                    <td>{{ $patient->waterType->value() }}</td>
-                    <td>{{ $patient->has_refrigerator }}</td>
-                    <td>{{ $patient->has_electricity }}</td>
-                    <td>{{ $patient->has_pet }}</td>
                     <td> {!! link_to_with_icon('fas fa-info-circle fa-2x', 'patients.show', $patient->id, '', 'has-text-info') !!} </td>
                     <td> {!! link_to_with_icon('fas fa-edit fa-2x', 'patients.edit', $patient, '', 'has-text-dark') !!} </td>
                     <td> {!! delete_link_with_icon('fas fa-trash-alt fa-2x', 'patients.destroy', $patient) !!} </td>
