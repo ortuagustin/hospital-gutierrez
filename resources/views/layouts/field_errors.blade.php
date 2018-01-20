c79e29995176ebe654267e54de04ed6ba252177c
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-20 16:14:16 -0300

    Partials: field_errors, 'container' wrapper is not needed

--- resources/views/layouts/field_errors.blade.php
+++ resources/views/layouts/field_errors.blade.php
@@ -1,13 +1,11 @@
 {{-- Will display validation errors for a given field  --}}
 
 @if ($errors->has($field))
-    <div class="container">
-        <ul>
-            @foreach ($errors->get($field) as $message)
-                <li class="help has-text-weight-bold has-text-danger">
-                    &nbsp; {{ $message }}
-                </li>
-            @endforeach
-        </ul>
-    </div>
+    <ul>
+        @foreach ($errors->get($field) as $message)
+            <li class="help has-text-weight-bold has-text-danger">
+                &nbsp; {{ $message }}
+            </li>
+        @endforeach
+    </ul>
 @endif
