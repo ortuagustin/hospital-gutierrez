a932689f98e82900e780296261cfe5b42af9aa50
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-20 17:00:05 -0300

    Codestyle

--- resources/views/permissions/_form.blade.php
+++ resources/views/permissions/_form.blade.php
@@ -1,20 +1,25 @@
 {{ csrf_field() }}
 
 <div class="field">
-  <label class="label">Permission</label>
-  <div class="control">
-    <input class="input" type="text" name="name" value="{{ old('name', $permission->name) }}" placeholder="The Permission name">
-    @include('layouts.field_errors', ['errors,' => 'errors', 'field' => 'name'])
-  </div>
+
+    <label class="label">Permission</label>
+
+    <div class="control">
+        <input class="input" type="text" name="name" value="{{ old('name', $permission->name) }}" placeholder="The Permission name">
+
+        @include('layouts.field_errors', ['errors,' => 'errors', 'field' => 'name'])
+    </div>
+
 </div>
 
 <div class="field is-grouped">
-  <div class="control">
-    <button type="submit" class="button is-primary">
-        {{ $submitButtonText }}
-    </button>
-  </div>
-  <div class="control">
-      {!! link_to('Cancel', 'patients.index', [], 'button is-danger is-outlined') !!}
-  </div>
+
+    <div class="control">
+        <button type="submit" class="button is-primary">{{ $submitButtonText }}</button>
+    </div>
+
+    <div class="control">
+        {!! link_to('Cancel', 'patients.index', [], 'button is-danger is-outlined') !!}
+    </div>
+
 </div>
