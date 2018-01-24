6d0324752f7950319916a89ac56ff4e6f4ba2db2
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-24 01:46:48 -0300

    Added $class slot for child-tile component

--- resources/views/components/home/child-tile.blade.php
+++ resources/views/components/home/child-tile.blade.php
@@ -1,5 +1,5 @@
 <div class="tile is-parent">
-    <article class="tile is-child box notification {{ $type or '' }}">
+    <article class="tile is-child box notification {{ $class or '' }} {{ $type or '' }}">
 
         <p class="title">{{ $title }}</p>
         <p class="subtitle">{{ $subtitle }}</p>
