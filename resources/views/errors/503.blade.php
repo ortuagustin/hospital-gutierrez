6567e327ef8f719a10c76eda13c782c0818f6f00
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-27 18:50:17 -0300

    503 View:: Changing the message if the user is a guest or a normal, authenticated user, without admin role

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,6 +8,14 @@
     <div class="container has-text-centered">
         <h1 class="title">Sorry, We're under Maintenance</h1>
         <h2 class="subtitle">We'll be back soon!</h2>
-        <p class="subtitle is-5">If you're an Admin, you may log in and {!!link_to('access the site', 'login', []) !!}</p>
+
+        @guest
+            <p class="subtitle is-5">If you're an Administrator, you may log in and {!!link_to('access the site', 'login', []) !!}</p>
+        @endguest
+
+        @auth
+            <p class="subtitle is-5">Please contact an Administrator for details</p>
+        @endauth
+
     </div>
 @endsection
