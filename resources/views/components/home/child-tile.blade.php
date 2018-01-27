3fbdf6ae876622d36c6669378ac4248597b9ffd2
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-27 01:14:37 -0300

    child-tile component: make the title and subtitle slots optional

--- resources/views/components/home/child-tile.blade.php
+++ resources/views/components/home/child-tile.blade.php
@@ -1,8 +1,8 @@
 <div class="tile is-parent">
     <article class="tile is-child box notification {{ $class or '' }} {{ $type or '' }}">
 
-        <p class="title">{{ $title }}</p>
-        <p class="subtitle">{{ $subtitle }}</p>
+        <p class="title">{{ $title or '' }}</p>
+        <p class="subtitle">{{ $subtitle or '' }}</p>
 
         <div class="content">
             {{ $slot }}
