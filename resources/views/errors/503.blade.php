23b3033bb1117ae81df867f5bebfb78c94e7463b
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-27 18:28:31 -0300

    Added link to login in 503 View

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,5 +8,6 @@
     <div class="container has-text-centered">
         <h1 class="title">Sorry, We're under Maintenance</h1>
         <h2 class="subtitle">We'll be back soon!</h2>
+        <p class="subtitle is-5">If you're an Admin, you may log in and {!!link_to('access the site', 'login', []) !!}</p>
     </div>
 @endsection
