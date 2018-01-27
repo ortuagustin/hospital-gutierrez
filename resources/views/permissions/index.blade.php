4c43314565365f69d592b8fd158ae99a893e8150
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-27 01:43:07 -0300

    Improved navigation on PermissionsController@index

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -5,21 +5,19 @@
     <h1 class="title is-3 has-text-grey">Permissions List</h1>
 
     <div class="box">
-        {!! link_to_with_icon('fas fa-plus fa-lg', 'permissions.create', [], 'Create a new Permission', 'has-text-success') !!}
+        {!! link_to_with_icon('fas fa-arrow-left fa-2x', 'home', [], 'Back to Home', 'has-text-info') !!}
     </div>
 
     {{ $permissions->links('pagination._header') }}
 
     <div class="box">
 
-        <table class="table is-striped is-narrow">
+        <table class="table is-striped is-narrow is-fullwidth">
 
             <thead>
                 <tr>
-                    <th><abbr title="Number">#</abbr></th>
+                    <th>ID</th>
                     <th>Name</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
                 </tr>
             </thead>
 
@@ -28,8 +26,6 @@
                     <tr>
                         <th>{{ $permission->id }}</th>
                         <td>{{ $permission->name }}</td>
-                        <td> {!! link_to_with_icon('fas fa-edit fa-2x', 'permissions.edit', $permissions, '', 'has-text-dark') !!} </td>
-                        <td> {!! delete_link_with_icon('fas fa-trash-alt fa-2x', 'permissions.destroy', $permissions) !!} </td>
                     </tr>
                 @endforeach
             </tbody>
