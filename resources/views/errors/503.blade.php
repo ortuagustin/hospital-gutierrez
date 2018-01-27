ce49aabe92f5a0388e0fb607b1e7b32df67c0dc4
Agustin Ortu <ortu.agustin@gmail.com>
2018-01-27 18:18:37 -0300

    Added custom maintenance mode view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('styles')
+    <link rel="stylesheet" href="/css/503.css">
+@endsection
+
+@section('content')
+    <div class="container has-text-centered">
+        <h1 class="title">Sorry, We're under Maintenance</h1>
+        <h2 class="subtitle">We'll be back soon!</h2>
+    </div>
+@endsection
