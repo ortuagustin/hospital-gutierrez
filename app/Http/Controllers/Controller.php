e6202e926be5ca2a813ffc076ca3ebe7aeac7a98
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-10 18:50:30 -0300

    Fixing issue with authorization on @update actions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
-    use AuthorizesRequests { resourceAbilityMap as resourceAbilityMapTrait; }
+    use AuthorizesRequests {
+        resourceAbilityMap as resourceAbilityMapTrait;
+        resourceMethodsWithoutModels as resourceMethodsWithoutModelsTrait; }
 
     /**
      * @inheritDoc
@@ -20,5 +22,16 @@ class Controller extends BaseController
         // this will make the index action of controllers use the policy method "view" for authorizations
         // based on https://github.com/laravel/internals/issues/772#issuecomment-327643505
         return array_merge($this->resourceAbilityMapTrait(), ['index' => 'view']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function resourceMethodsWithoutModels()
+    {
+        // in \App\Policies\ModelPolicy it's defined that the update action does not require the model instance
+        // if this method weren't overriden, then controllers MUST add the model parameter in the method definition
+        // this allows to only have the Request parameter in controllers
+        return array_merge($this->resourceMethodsWithoutModelsTrait(), ['update']);
     }
 }
