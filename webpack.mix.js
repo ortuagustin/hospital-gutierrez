71cb14792dba615c33ec3ee76c7213694e903eec
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-23 02:25:29 -0300

    Using laravel-mix to bundle all css files in app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,4 +11,8 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js');
+mix.js('resources/assets/js/app.js', 'public/js')
+  .styles([
+    'resources/assets/css/app.css',
+    'resources/assets/css/animate.css'
+  ], 'public/css/app.css');
