d482c4697995c76c4179b1c5a80b122a324f4f3d
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-23 00:55:51 -0300

    - Removed UNLP logo from footer - Added VueJS to footer - Added Bulma CSS to footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,27 +2,37 @@
 
     <div class="level-item has-text-centered">
         <div>
-            <a href="https://laravel.com">{!! icon('fab fa-laravel fa-4x', 'has-text-danger') !!}</a>
-            <p class="title is-4">Laravel</p>
-            <p class="heading">Framework PHP</p>
+            <a href="https://laravel.com">{!! icon('fab fa-laravel fa-4x', 'has-text-danger') !!}
+                <p class="title is-4">Laravel</p>
+                <p class="heading">Framework PHP</p>
+            </a>
         </div>
     </div>
 
     <div class="level-item has-text-centered">
         <div>
-            <a href="https://github.com/ortuagustin/promo_proyecto">{!! icon('fab fa-github fa-4x', 'has-text-dark') !!}</a>
-            <p class="title is-4">Ortu, Agust&iacute;n</p>
-            <p class="heading">C&oacute;digo Fuente</p>
+            <a href="https://vuejs.org">{!! icon('fab fa-vuejs fa-4x', 'has-text-success') !!}
+                <p class="title is-4">Vue</p>
+                <p class="heading">Framework JavaScript</p>
+            </a>
         </div>
     </div>
 
     <div class="level-item has-text-centered">
         <div>
-            <a href="http://info.unlp.edu.ar/">
-                <figure class="image">
-                    <img src="https://www.guarani-informatica.unlp.edu.ar/_comp/pack_comunes/img/logo-transparente.png" alt="" style="height: 96px;">
-                </figure>
-                <p class="heading">&nbsp;</p>
+            <a href="https://vuejs.org">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="BULMA CSS" width="200" height="200">
+                <p class="title is-6">&nbsp;</p>
+                <p class="heading">Framework CSS</p>
+            </a>
+        </div>
+    </div>
+
+    <div class="level-item has-text-centered">
+        <div>
+            <a href="https://github.com/ortuagustin/promo_proyecto">{!! icon('fab fa-github fa-4x', 'has-text-dark') !!}
+                <p class="title is-4">Ortu, Agust&iacute;n</p>
+                <p class="heading">C&oacute;digo Fuente</p>
             </a>
         </div>
     </div>
