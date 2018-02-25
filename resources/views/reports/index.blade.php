2738ab0c356b6aed8c1388d92740453f29d75a07
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-24 23:32:06 -0300

    Reports@index: charts should be displayed in groups of 3

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 
 @foreach ($reports->chunk(3) as $chunk)
-    @foreach ($chunk as $each)
-        <div class="columns">
+    <div class="columns">
+        @foreach ($chunk as $each)
             <div class="column is-one-third">
                 <pie-chart
                     endpoint={{ $each->endpoint() }}
                     title="{{ $each->title() }}">
                 </pie-chart>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+</div>
 @endforeach
 
 @endsection
