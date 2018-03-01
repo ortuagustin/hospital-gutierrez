b216434f2547f71f521da4a5cf39eac3e05e4e53
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-01 15:04:00 -0300

    Added signIn() methods to FeatureTest

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -2,14 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\CreatesApplication;
+use Tests\Helpers\UserTestHelper;
 use Tests\RunsMigrations;
 
 abstract class FeatureTest extends BaseTestCase
 {
-    use CreatesApplication;
-    use RunsMigrations;
-    use DatabaseTransactions;
+    use CreatesApplication, RunsMigrations, DatabaseTransactions, UserTestHelper;
+
+    protected function signIn(User $user = null)
+    {
+        $user = $user ?: $this->createUser();
+        $this->be($user);
+
+        return $this;
+    }
+
+    protected function signInAdmin(User $user = null)
+    {
+        $user = $user ?: $this->createAdminUser();
+
+        return $this->signIn($user);
+    }
 }
