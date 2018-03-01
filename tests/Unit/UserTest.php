57897e2285ba135a50e3f5a50a46e833fed04745
Agustin Ortu <ortu.agustin@gmail.com>
2018-02-28 23:58:40 -0300

    Added roles test to User model

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -12,9 +12,50 @@ class UserTest extends TestCase
     use RoleTestHelper;
 
     /** @test */
+    public function it_has_many_roles()
+    {
+        $user = $this->createUser();
+        $this->assertEmpty($user->roles);
+
+        $user->roles()->attach($this->createRole('Some Role'));
+        $this->assertEquals(1, $user->roles()->count());
+
+        $user->roles()->attach($this->createRole('Other Role'));
+        $this->assertEquals(2, $user->roles()->count());
+    }
+
+    /** @test */
+    public function it_can_delete_any_associated_role()
+    {
+        $user = $this->createUser();
+        $user->roles()->attach($role = $this->createRole('Some Role'));
+        $this->assertEquals(1, $user->roles()->count());
+
+        $user->roles()->detach($role);
+
+        $this->assertEmpty($user->roles);
+    }
+
+    /** @test */
+    public function it_can_delete_any_associated_roles()
+    {
+        $user = $this->createUser();
+        $user->roles()->attach($role1 = $this->createRole('Some Role'));
+        $user->roles()->attach($role2 = $this->createRole('Another Role'));
+        $this->assertEquals(2, $user->roles()->count());
+
+        $user->roles()->detach($role1);
+
+        $this->assertEquals(1, $user->roles()->count());
+        $this->assertTrue($user->hasNotRole($role1));
+        $this->assertTrue($user->hasRole($role2));
+    }
+
+    /** @test */
     public function it_returns_roles_separated_by_comma()
     {
         $user = $this->createUser();
+
         $user->roles()->attach($this->createRole('Admin'));
         $user->roles()->attach($this->createRole('Guest'));
         $user->roles()->attach($this->createRole('Medic'));
@@ -34,6 +75,7 @@ class UserTest extends TestCase
     public function it_returns_one_role_when_user_has_only_one_role()
     {
         $user = $this->createUser();
+
         $user->roles()->attach($this->createRole('Admin'));
 
         $this->assertEquals('Admin', $user->roles_names());
@@ -43,6 +85,7 @@ class UserTest extends TestCase
     public function it_trims_the_string_if_more_than_three_roles()
     {
         $user = $this->createUser();
+
         $user->roles()->attach($this->createRole('Admin'));
         $user->roles()->attach($this->createRole('Medic'));
         $user->roles()->attach($this->createRole('Receptionist'));
