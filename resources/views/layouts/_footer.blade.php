72ed0087012bb0f3d2deba8a750e7a94a03ba9da
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-02 20:09:01 -0300

    Added Algolia Logo

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -30,6 +30,16 @@
 
     <div class="level-item has-text-centered">
         <div>
+            <a href="https://www.algolia.com/">
+                <img src="https://www.algolia.com/static_assets/images/press/downloads/algolia-logo-light.png" alt="Algolia" width="200" height="200">
+                <p class="title is-6">&nbsp;</p>
+                <p class="heading">Search Engine</p>
+            </a>
+        </div>
+    </div>
+
+    <div class="level-item has-text-centered">
+        <div>
             <a href="https://github.com/ortuagustin/promo_proyecto">{!! icon('fab fa-github fa-4x', 'has-text-dark') !!}
                 <p class="title is-4">Ortu, Agust&iacute;n</p>
                 <p class="heading">C&oacute;digo Fuente</p>
