7f7ece9e86551e948eeabb3aa1179d1269b35ca6
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-03 17:03:10 -0300

    Extracted patients index/search header section to partial

---
+++ resources/views/patients/_list_header.blade.php
@@ -0,0 +1,13 @@
+<div class="box level">
+    <div class="level-left">
+        {!! link_to_with_icon('fas fa-arrow-left fa-2x', 'home', [], 'Back to Home', 'has-text-info') !!}
+    </div>
+
+    @can ('create', \App\Patient::class)
+        <div class="level-right">
+            <div class="level-item">
+                {!! link_to_with_icon('fas fa-plus fa-2x', 'patients.create', [], 'Create a new Patient', 'has-text-success') !!}
+            </div>
+        </div>
+    @endcan
+</div>
--- resources/views/patients/search.blade.php
+++ resources/views/patients/search.blade.php
@@ -1,25 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1 class="title is-3 has-text-grey">Patient List</h1>
+    <h1 class="title is-3 has-text-grey">Patient Search</h1>
 
-    <div class="box level">
-
-        <div class="level-left">
-            {!! link_to_with_icon('fas fa-arrow-left fa-2x', 'home', [], 'Back to Home', 'has-text-info') !!}
-        </div>
-
-        @can ('create', \App\Patient::class)
-            <div class="level-right">
-
-                <div class="level-item">
-                    {!! link_to_with_icon('fas fa-plus fa-2x', 'patients.create', [], 'Create a new Patient', 'has-text-success') !!}
-                </div>
-
-            </div>
-        @endcan
-
-    </div>
+    @include('patients._list_header')
 
     <div id="app">
         <ais-index
