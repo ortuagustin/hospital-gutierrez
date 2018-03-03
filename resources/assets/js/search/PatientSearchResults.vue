472b72968ec4b4d937f18afc8bb4ed0f23f2535e
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-03 13:47:53 -0300

    PatientsSearchResult: added display when no results are returned

--- resources/assets/js/search/PatientSearchResults.vue
+++ resources/assets/js/search/PatientSearchResults.vue
@@ -1,13 +1,21 @@
 <template>
-  <ais-results>
-      <template slot-scope="{ result }">
-          <p>
-              <a :href="result.path" class="is-size-5">
-                  <ais-highlight :result="result" :attribute-name="attributeName"></ais-highlight>
-              </a>
-          </p>
-      </template>
-  </ais-results>
+<div>
+    <ais-results>
+        <template slot-scope="{ result }">
+            <p>
+                <a :href="result.path" class="is-size-5">
+                    <ais-highlight :result="result" :attribute-name="attributeName"></ais-highlight>
+                </a>
+            </p>
+        </template>
+    </ais-results>
+
+    <ais-no-results>
+        <template slot-scope="props">
+            No Patients found <span v-if="props.query"> for <b>{{ props.query }}</b></span>
+        </template>
+    </ais-no-results>
+</div>
 </template>
 
 <script>
