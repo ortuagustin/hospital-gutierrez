441d8a206f9eee370adc7bf686a7fed4b2f37d0a
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-03 01:44:48 -0300

    Created some wrapper Instant Search Components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
+import InstantSearch from 'vue-instantsearch';
 import UserRoleSelect from './components/UserRoleSelect.vue';
+import SearchTypeFilter from './search/SearchTypeFilter.vue';
+import PatientSearchResults from './search/PatientSearchResults.vue';
+
+Vue.use(InstantSearch);
 
 const app = new Vue({
-  components: { UserRoleSelect },
+    components: { UserRoleSelect, SearchTypeFilter, PatientSearchResults },
 });
 
 if (document.getElementById('app')) {
-  app.$mount('#app');
+    app.$mount('#app');
 }
---
+++ resources/assets/js/search/PatientSearchResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <ais-results>
+      <template slot-scope="{ result }">
+          <p>
+              <a :href="result.path" class="is-size-5">
+                  <ais-highlight :result="result" :attribute-name="attributeName"></ais-highlight>
+              </a>
+          </p>
+      </template>
+  </ais-results>
+</template>
+
+<script>
+import Results from "vue-instantsearch";
+
+export default {
+  props: ["attributeName"],
+
+  components: { Results }
+};
+</script>
+
+<style>
+.ais-highlight > em {
+  background: yellow;
+  font-weight: bold;
+}
+</style>
