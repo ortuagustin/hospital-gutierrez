f43a78512f2ba521cba728ef5be845030b4c14e2
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-06 11:49:09 -0300

    removed GET /register route

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
 
 Route::middleware('auth')->group(function () {
