d62b52ea7d7e0683b4b39d8be63d5f7146ce0c98
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-07 19:29:25 -0300

    Added hr to pagination views

--- resources/views/pagination/_footer.blade.php
+++ resources/views/pagination/_footer.blade.php
@@ -1,7 +1,7 @@
 @if ($paginator->hasPages())
+    <hr>
 
     <nav class="pagination is-centered">
         @include('pagination._content')
     </nav>
-
 @endif
--- resources/views/pagination/_header.blade.php
+++ resources/views/pagination/_header.blade.php
@@ -1,7 +1,7 @@
 @if ($paginator->hasPages())
-
-    <nav class="box pagination is-centered">
+    <nav class="pagination is-centered">
         @include('pagination._content')
     </nav>
 
+    <hr>
 @endif
