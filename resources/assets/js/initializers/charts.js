25be52a605957e4c4f79ff062f27e7d415dd2254
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-07 20:34:30 -0300

    Fixed naming issue with chart components

--- resources/assets/js/initializers/charts.js
+++ resources/assets/js/initializers/charts.js
@@ -3,6 +3,6 @@ import PieChart from '../components/PieChart.vue';
 import BarChart from '../components/BarChart.vue';
 import LineChart from '../components/LineChart.vue';
 
-Vue.component('pie-chart', PieChart);
-Vue.component('bar-chart', BarChart);
-Vue.component('line-chart', LineChart);
+Vue.component('vue-pie-chart', PieChart);
+Vue.component('vue-bar-chart', BarChart);
+Vue.component('vue-line-chart', LineChart);
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -5,10 +5,10 @@
 <div class="columns is-multiline is-centered">
     @foreach ($reports as $each)
         <div class="column is-one-third">
-            <pie-chart
+            <vue-pie-chart
                 endpoint={{ $each->endpoint() }}
                 title="{{ $each->title() }}">
-            </pie-chart>
+            </vue-pie-chart>
         </div>
     @endforeach
 </div>
