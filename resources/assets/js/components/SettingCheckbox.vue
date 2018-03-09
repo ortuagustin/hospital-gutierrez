1a7f53019718df0ed6ae810c26163f386a8df1d8
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-09 18:44:25 -0300

    SettingCheckbox component now renders as a Switch

--- resources/assets/js/components/SettingCheckbox.vue
+++ resources/assets/js/components/SettingCheckbox.vue
@@ -5,16 +5,13 @@
   </div>
 
   <div class="column">
-      <div class="field is-grouped">
-        <p class="control is-expanded">
-          <label class="checkbox">
-            <input ref="input" name="key" type="checkbox" v-model="checked" @change="changed" :disabled="loading">
+    <div class="field">
+        <b-switch v-model="form.value" true-value="1" false-value="0" @input="changed">
+          <slot></slot>
+        </b-switch>
+    </div>
 
-            <slot></slot>
-          </label>
-          <span v-if="feedback" v-text="feedback" class="help has-text-weight-bold has-text-danger"></span>
-        </p>
-      </div>
+    <span v-if="feedback" v-text="feedback" class="help has-text-weight-bold has-text-danger"></span>
   </div>
 </div>
 </template>
@@ -25,18 +22,11 @@ import SettingInput from "./SettingInput.vue";
 export default {
   extends: SettingInput,
 
-  mounted() {
-    this.$refs.input.checked = this.setting.value == "1";
-  },
-
   methods: {
-    checked() {
-      return this.form.value ? "1" : "0";
-    },
-
     changed(event) {
-      this.form.value = event.target.checked ? "1" : "0";
-      this.submit();
+      if (this.setting.value !== this.form.value) {
+        this.submit();
+      }
     }
   }
 };
--- resources/assets/js/initializers/buefy.js
+++ resources/assets/js/initializers/buefy.js
@@ -4,6 +4,7 @@ import Toast from 'buefy/src/components/toast';
 import Dialog from 'buefy/src/components/dialog';
 import Collapse from 'buefy/src/components/collapse';
 import Icon from 'buefy/src/components/icon';
+import Switch from 'buefy/src/components/switch';
 
 Vue.prototype.$toast = Toast;
 Vue.prototype.$dialog = Dialog;
@@ -22,3 +23,4 @@ Vue.prototype.confirm = function(title, message, type, onConfirm, onCancel) {
 
 Vue.component('b-collapse', Collapse);
 Vue.component('b-icon', Icon);
+Vue.component('b-switch', Switch);
