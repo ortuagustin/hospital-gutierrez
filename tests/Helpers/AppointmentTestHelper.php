cab30280fc2e144e93134b12e0a0b20457f6cf53
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-10 07:51:15 -0300

    add createAppointmentAt to AppointmentTestHelper

--- tests/Helpers/AppointmentTestHelper.php
+++ tests/Helpers/AppointmentTestHelper.php
@@ -3,11 +3,13 @@
 namespace Tests\Helpers;
 
 use App\Appointment;
+use Carbon\Carbon;
 
 trait AppointmentTestHelper
 {
     /**
      * Saves a new Appointment to the database and returns it
+     *
      * @param array $overrides
      *
      * @return Appointment
@@ -19,6 +21,7 @@ trait AppointmentTestHelper
 
     /**
      * Saves a collection of new Appointments to the database and returns it
+     *
      * @param int $amount
      * @param array $overrides
      *
@@ -27,5 +30,21 @@ trait AppointmentTestHelper
     protected function createAppointments($amount = 2, array $overrides = [])
     {
         return factory(Appointment::class, $amount)->create($overrides);
+    }
+
+    /**
+     * Saves a new Appointment at the given date to the database and returns it
+     *
+     * @param int|null  $hour
+     * @param int|null  $minute
+     * @param int|null  $day
+     * @param int|null  $month
+     * @param int|null  $year
+     *
+     * @return Appointment
+     */
+    protected function createAppointmentAt($hour = null, $minute = null, $day = null, $month = null, $year = null)
+    {
+        return factory(Appointment::class)->create(['date' => Carbon::create($year, $month, $day, $hour, $minute)]);
     }
 }
