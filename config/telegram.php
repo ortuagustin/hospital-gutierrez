c8df29522b7b494a84174fe0287ad2d98135eacd
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-10 19:25:14 -0300

    Created telegram config file

---
+++ config/telegram.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'key'           => env('TELEGRAM_BOT_API_KEY'),
+  'bot_username'  => env('TELEGRAM_BOT_USERNAME'),
+  'bot_hook_url'  => env('TELEGRAM_BOT_HOOK_URL'),
+  'api_base_url'  => env('TELEGRAM_BOT_API_BASE_URL'),
+];
