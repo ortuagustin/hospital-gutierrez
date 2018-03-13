85f08c2f04ab957032240e351735cb21a29dbe99
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-13 15:23:44 -0300

    Codestyle

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ Route::middleware('auth')->group(function () {
     Route::resource('reports', 'DemographicReportsController', ['only' => ['index', 'show']]);
 
     Route::middleware('can:admin')->group(function () {
-        Route::get('/roles/reset', 'RolesController@reset')->name('roles.reset');
+        Route::get('roles/reset', 'RolesController@reset')->name('roles.reset');
         Route::resource('roles', 'RolesController');
         Route::resource('permissions', 'PermissionsController', ['only' => ['index']]);
         Route::resource('users', 'UsersController', ['only' => ['index', 'show', 'destroy']]);
         Route::delete('user/{user}/roles/{role}', 'UserRolesController@destroy')->name('user.roles.destroy');
         Route::patch('user/{user}/roles', 'UserRolesController@update')->name('user.roles.update');
-        Route::delete('/settings/reset', 'SettingsController@reset')->name('settings.reset');
+        Route::delete('settings/reset', 'SettingsController@reset')->name('settings.reset');
         Route::resource('settings', 'SettingsController', ['only' => ['index', 'store']]);
     });
 });
