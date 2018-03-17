bf07ba542cf14578cf91f54ad084bc50b533e40d
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-17 18:33:23 -0300

    Improved styling of patients search result list

--- resources/assets/js/algolia/PatientSearchResults.vue
+++ resources/assets/js/algolia/PatientSearchResults.vue
@@ -1,17 +1,17 @@
 <template>
-<div>
-    <ais-results>
-        <template slot-scope="{ result }">
-            <slot :patient="result"></slot>
-        </template>
-    </ais-results>
+    <div class="results-container">
+        <ais-results>
+            <template slot-scope="{ result }">
+                <slot :patient="result"></slot>
+            </template>
+        </ais-results>
 
-    <ais-no-results>
-        <template slot-scope="props">
-            No Patients found <span v-if="props.query"> for <b>{{ props.query }}</b></span>
-        </template>
-    </ais-no-results>
-</div>
+        <ais-no-results>
+            <template slot-scope="props">
+                No Patients found <span v-if="props.query"> for <b>{{ props.query }}</b></span>
+            </template>
+        </ais-no-results>
+    </div>
 </template>
 
 <script>
@@ -27,5 +27,9 @@ export default {
 .ais-highlight > em {
   background: yellow;
   font-weight: bold;
+}
+
+.results-container {
+  margin-top: 3.5rem;
 }
 </style>
