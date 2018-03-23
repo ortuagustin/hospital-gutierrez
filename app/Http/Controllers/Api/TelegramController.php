ac380a505351eba847f80911afae62841811b6a8
Agustin Ortu <ortu.agustin@gmail.com>
2018-03-23 19:07:53 -0300

    Added TelegramController

---
+++ app/Http/Controllers/Api/TelegramController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+class TelegramController extends Controller
+{
+    public function handle()
+    {
+        try {
+            Telegram::commandsHandler(true);
+        } catch (\Exception $e) {
+            return response($e->getMessage(), 200);
+        }
+
+        return response('ok', 200);
+    }
+
+    public function setWebhook()
+    {
+        return Telegram::setWebhook([
+                'url'   => config('app.url') . '/api/telegram',
+            ]);
+    }
+
+    public function removeWebhook()
+    {
+        Telegram::removeWebhook();
+
+        return response('webhook removed!', 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
+Route::post("telegram", 'Api\TelegramController@handle')->name('telegram');
+Route::get("telegram/setWebhook", 'Api\TelegramController@setWebhook');
+Route::get("telegram/removeWebhook", 'Api\TelegramController@removeWebhook');
 Route::get('turnos/{fecha}', 'Api\ApointmentsController@index');
 Route::post('turnos', 'Api\ApointmentsController@store');
